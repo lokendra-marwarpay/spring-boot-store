482e05531621056218cbabd1c6dd285707583287
Lokendra Gupta <lokendra.marwarpay@gmail.com>
2025-07-21 17:39:00 +0530

    done till 30

--- src/main/java/com/codewithmosh/store/controllers/ProductController.java
+++ src/main/java/com/codewithmosh/store/controllers/ProductController.java
@@ -3,18 +3,24 @@ package com.codewithmosh.store.controllers;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.codewithmosh.store.dtos.ProductDto;
 import com.codewithmosh.store.entities.Product;
 import com.codewithmosh.store.mappers.ProductMapper;
+import com.codewithmosh.store.repositories.CategoryRepository;
 import com.codewithmosh.store.repositories.ProductRepository;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
 @AllArgsConstructor
@@ -22,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class ProductController {
     private ProductRepository productRepository;
     private ProductMapper productMapper;
+    private CategoryRepository categoryRepository;
 
     @GetMapping
     public Iterable<ProductDto> getAllProducts(@RequestParam(name = "categoryId", required = false) Byte categoryId) {
@@ -46,4 +53,49 @@ public class ProductController {
         return ResponseEntity.ok(productMapper.toDto(product));
     }
 
+    @PostMapping
+    public ResponseEntity<ProductDto> createProduct(@RequestBody ProductDto request,
+            UriComponentsBuilder uriComponentsBuilder) {
+        var category = categoryRepository.findById(request.getCategoryId()).orElse(null);
+        if (category == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        var product = productMapper.toEntity(request);
+        product.setCategory(category);
+        productRepository.save(product);
+        var productDto = productMapper.toDto(product);
+        var uri = uriComponentsBuilder.path("/products/{id}").buildAndExpand(productDto.getId()).toUri();
+        return ResponseEntity.created(uri).body(productDto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ProductDto> updateProduct(@RequestBody ProductDto request,
+            @PathVariable Long id) {
+        var product = productRepository.findById(id).orElse(null);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+        var category = categoryRepository.findById(request.getCategoryId()).orElse(null);
+        if (category == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        productMapper.update(request, product);
+        product.setCategory(category);
+        productRepository.save(product);
+        request.setId(id);
+
+        return ResponseEntity.ok(request);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        var product = productRepository.findById(id).orElse(null);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        productRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
 }
--- src/main/java/com/codewithmosh/store/mappers/ProductMapper.java
+++ src/main/java/com/codewithmosh/store/mappers/ProductMapper.java
@@ -2,6 +2,7 @@ package com.codewithmosh.store.mappers;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 import com.codewithmosh.store.dtos.ProductDto;
 import com.codewithmosh.store.entities.Product;
@@ -10,4 +11,7 @@ import com.codewithmosh.store.entities.Product;
 public interface ProductMapper {
     @Mapping(source = "category.id", target = "categoryId")
     ProductDto toDto(Product product);
+    Product toEntity(ProductDto productDto);
+    @Mapping(target = "id", ignore = true)
+    void update(ProductDto productDto, @MappingTarget Product product);
 }
